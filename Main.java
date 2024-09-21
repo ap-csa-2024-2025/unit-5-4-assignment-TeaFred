6fbb385ccca45c2a14b7768df01c496b225af57e
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-21 01:16:29 +0000

    Completed assignment

--- Main.java
+++ Main.java
@@ -2,12 +2,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    System.out.println(DistanceFormula(0,5,0,0));
+    System.out.println(AddingSqrt (1,0,-4));
+    System.out.println(SubtractingSqrt (1,1,-6));
+
   }
-
-  // Implement distance formula method here
-
-  // implement one of your quadratic formula methods here
-
-  // implement the other quadratic formula here
+  public static double DistanceFormula(int a, int b, int c, int d)
+  {
+    return Math.sqrt((a-c) * (a-c) + (b-d) * (b-d));
+  }
+  public static double AddingSqrt(double a, double b, double c)
+  {
+    return ((-1 * b) + Math.sqrt(b*b - 4*a*c ) )/(2*a); 
+  }
+  public static double SubtractingSqrt(double a, double b, double c)
+  {
+  return ((-1 * b) - Math.sqrt(b*b - 4*a*c ) )/(2*a);
+  }
 }
